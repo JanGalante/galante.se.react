9b5b05f3c2bb35a4f8b62243175753ffdb18ac44
Jan Lundholm Galante <jan.lundholm.galante@thomascook.se>
2018-03-22 12:33:31 +0100

    New logo and improved menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,25 +5,32 @@ import logo from "../logo.svg";
 const listStyle = {
   color: "aqua"
 };
+const logoStyle = {
+  "font-size": "60px",
+  "font-family": "Georgia"
+};
 
 // class Header extends Component {
 const Header = () => {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to Galante</h1>
-      <ul>
-        <li style={listStyle}>
-          <Link to="/">Home</Link>
-        </li>
-        <li style={listStyle}>
-          <Link to="/about">About</Link>
-        </li>
-        <li style={listStyle}>
-          <Link to="/topics">Topics</Link>
-        </li>
-      </ul>
-    </header>
+    <div>
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div style={logoStyle}>J|G</div>
+        <h1 className="App-title">Welcome to Galante</h1>
+        <ul className="menu">
+          <li style={listStyle}>
+            <Link to="/">Home</Link>
+          </li>
+          <li style={listStyle}>
+            <Link to="/about">About</Link>
+          </li>
+          <li style={listStyle}>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+      </header>
+    </div>
   );
 };
 

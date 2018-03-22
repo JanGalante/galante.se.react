e02be5036b28aa8b2656f4ffffa853bdc9418eed
Jan Lundholm Galante <jan.lundholm.galante@thomascook.se>
2018-03-22 11:34:47 +0100

    Change header to statless component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 import logo from "../logo.svg";
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-    );
-  }
-}
+const listStyle = {
+  color: "aqua"
+};
+
+// class Header extends Component {
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to Galante</h1>
+      <ul>
+        <li style={{ color: "red" }}>
+          <Link to="/">Home</Link>
+        </li>
+        <li style={listStyle}>
+          <Link to="/about">About</Link>
+        </li>
+        <li style={listStyle}>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
 
 export default Header;

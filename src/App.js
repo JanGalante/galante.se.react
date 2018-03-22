b2b767ad7329e22f6a77caa84f4d52f00992c188
Jan Lundholm Galante <jan.lundholm.galante@thomascook.se>
2018-03-22 10:32:38 +0100

    Suppot for Twitter Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -19,3 +19,41 @@ class App extends Component {
 }
 
 export default App;
+
+// import React, { Component } from "react";
+// import { Grid, Navbar, Jumbotron, Button } from "react-bootstrap";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Navbar inverse fixedTop>
+//           <Grid>
+//             <Navbar.Header>
+//               <Navbar.Brand>
+//                 <a href="/">React App</a>
+//               </Navbar.Brand>
+//               <Navbar.Toggle />
+//             </Navbar.Header>
+//           </Grid>
+//         </Navbar>
+//         <Jumbotron>
+//           <Grid>
+//             <h1>Welcome to React</h1>
+//             <p>
+//               <Button
+//                 bsStyle="success"
+//                 bsSize="large"
+//                 href="http://react-bootstrap.github.io/components.html"
+//                 target="_blank">
+//                 View React Bootstrap Docs
+//               </Button>
+//             </p>
+//           </Grid>
+//         </Jumbotron>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;

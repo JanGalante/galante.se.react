3911ed7ad060fd3d74b9d3a8231b202f87272a99
Jan Lundholm Galante <jan.lundholm.galante@thomascook.se>
2019-07-09 23:16:00 +0200

    Fetch data from lambda functions

--- functions/sanity.js
+++ functions/sanity.js
@@ -1,7 +1,7 @@
 exports.handler = (event, context, callback) => {
   console.log(`Console log... Hola me llamo Lambda Paco`)
   return callback(null, {
-    statusCode: 400,
-    body: 'Hola me llamo Lambda Paco',
+    statusCode: 200,
+    body: 'Hola me llamo Paco Lambda',
   })
 }
--- src/views/Lambda.jsx
+++ src/views/Lambda.jsx
@@ -1,12 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { stringify } from 'querystring';
 
 const Lambda = () => {
-  const [result, setResult] = useState(async() => {
-    const response = await fetch('https://api.github.com/users/jangalante/repos')
-    const json = await response.json();
+  // const [result, setResult] = useState(async() => {
+  //   const response = await fetch('https://api.github.com/users/jangalante/repos')
+  //   const json = await response.json();
 
-    return 'hard coded result';
-  });
+  //   return 'hard coded result';
+  // });
+
+  // const [result, setResult] = useEffect(() => {
+  //   // async function
+
+  //   const response = await fetch('https://api.github.com/users/jangalante/repos')
+  //   const json = await response.json();
+
+  //   return 'hard coded result';
+  // });
+
+
+const [result, setResult] = useState();
+useEffect(() => {
+  // Effect callbacks are synchronous to prevent race conditions. Put the async function inside:
+  async function fetchData() {
+    // // You can await here
+    // const response = await fetch('https://api.github.com/users/jangalante/repos')
+    // const json = await response.json();
+
+    // const response = await fetch('https://galante.se/.netlify/functions/sanity');
+    const response = await fetch('./.netlify/functions/sanity');
+    const text = await response.text(); 
+    console.log('text', text);
+    setResult(text);
+    // ...
+  }
+  fetchData();
+}, []);  //[someId]); // Or [] if effect doesn't need props or state
+
+// Learn more about data fetching with Hooks: https://fb.me/react-hooks-data-fetchingeslint(react-hooks/exhaustive-deps)
+
 
   return (
     <div>

c821931244df6c95235b9854bac2594cbcb5e410
Jan Lundholm Galante <jan.lundholm.galante@thomascook.se>
2020-06-17 23:49:00 +0200

    Migrate react component to function with hooks

--- src/views/Repositories.jsx
+++ src/views/Repositories.jsx
@@ -1,60 +1,46 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import service from '../services/github'
 
-// TODO: Gör om till hooks och använd react-query för att hämta data och cache
+// // TODO: Gör om till hooks och använd react-query för att hämta data och cache
 
-export default class Repositories extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
+const Repositories = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { repositories, success } = await service.fetchRepositories();
+      setIsLoaded(true);
+      setItems(repositories);
+
+      if (!success) {
+        setError('Something went wron');
+      }
     };
+
+    fetchData();
+  }, [])
+
+  if (!isLoaded) {
+    return (<h1>loading...</h1>)
   }
 
-  async componentDidMount(){
-    const { repositories, success } = await service.fetchRepositories();
-    this.setState({
-      isLoaded: success,
-      items: repositories,
-    })
-
-    // Note: it's important to handle errors here
-    // instead of a catch() block so that we don't swallow
-    // exceptions from actual bugs in components.
-    // (error) => {
-    //   this.setState({
-    //     isLoaded: true,
-    //     error
-    //   });
-    // }
+  if (error) {
+    return (<p>Something went wrong when trying to get repositories from Github</p>)
   }
 
-  render() {
-    const isLoaded = this.state.isLoaded
-    const list = isLoaded ? (
+  return (
+    <div>
+      <h1>Mina respositories på Github</h1>
+      <p className="App-intro">Not so many topics as expected</p>
       <ul>
-        {this.state.items.map(item => (
-          <li key={item.id}>
-            {item.name}
-          </li>
+        {items.map(item => (
+          <li key={item.id}>{item.name}</li>
         ))}
       </ul>
-    ) : (
-        <p>Something went wrong when trying to get repositories from Github</p>
-    );
-
-    return (
-      <div>
-        <h1>Mina respositories på Github</h1>
-        <p className="App-intro">Not so many topics as expected</p>
-        <h2>Status</h2>
-        <p>
-          --- {this.state.isLoaded ? 'true': 'false'} ---
-        </p>
-        {list}
-      </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default Repositories;

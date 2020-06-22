3c0952bf92514fd77e67f154b64b871035a73f1e
Jan Lundholm Galante <jan.lundholm.galante@thomascook.se>
2020-06-22 11:55:04 +0200

    Bump and migrate http-proxy-middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,8 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  // ...
-  // app.use(proxy('/api', { target: 'http://localhost:5000/' }));
-  app.use(proxy('/.netlify/functions/', { 
+  app.use(createProxyMiddleware('/.netlify/functions/', { 
     target: 'http://localhost:9000/',
     "pathRewrite": {
       "^/\\.netlify/functions": ""

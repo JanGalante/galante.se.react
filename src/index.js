58fee69dc2010ea304289f1a1f2d21f3ba9fcde8
Jan Lundholm Galante <jan.lundholm.galante@thomascook.se>
2020-06-22 09:11:42 +0200

    Comment out bootstrap-theme.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css'; // Put any other imports below so that CSS from your components takes precedence over default styles.
+// import 'bootstrap/dist/css/bootstrap-theme.css'; // Put any other imports below so that CSS from your components takes precedence over default styles.
 import './index.css';
 
 import App from './App.jsx';

46d03bd4f11ae0d9d831c692d4b00b28447da7ef
Jan Lundholm Galante <jan.lundholm.galante@thomascook.se>
2020-07-01 13:09:32 +0200

    Use React function for Gravatar component

--- src/views/Gravatar.jsx
+++ src/views/Gravatar.jsx
@@ -1,26 +1,13 @@
-import React, { Component } from 'react'
-import md5 from 'md5'
+import React from "react";
+import md5 from "md5";
 
-export default class Gravatar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
-
- 
-
-  render() {
-    return (
-      <>
-        <h1>Gravatars</h1>
-        <p>
-        <img src={ `https://www.gravatar.com/avatar/${md5('ave_caesar@hotmail.com')}?s=50` } alt="avatar" /> ave_caesar@hotmail.com
-        </p>
-      </>
-    )
-  }
-}
+export default () => {
+  return (
+    <>
+      <h1>Gravatars</h1>
+      <p>
+      <img src={ `https://www.gravatar.com/avatar/${md5('ave_caesar@hotmail.com')}?s=50` } alt="avatar" /> ave_caesar@hotmail.com
+      </p>
+    </>
+  );
+};
